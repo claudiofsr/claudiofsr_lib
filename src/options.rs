33f60cb06c5e0b4c78b7814b11e95137d70dd2da
claudiofsr <claudiofsr@yahoo.com>
2026-08-13 15:52:17 -0300

    rename parse() to parse_opt()

--- src/options.rs
+++ src/options.rs
@@ -1,198 +1,170 @@
 use std::{fmt::Display, ops::Deref};
 
-/// Adds some methods to the `Option<T>`.
+/// Adds utility methods to the `Option<T>` type.
 pub trait OptionExtension<T> {
-    /**
-    Combine two Options with one operation.
-    ```
-    use claudiofsr_lib::OptionExtension;
-
-    let a = Some(5.0);
-    let b = Some(10.0);
-
-    let sum = |a, b| {a + b};
-    let sub = |a, b| {a - b};
-    let mul = |a, b| {a * b};
-    let div = |a, b| {a / b};
-
-    let result_sum = a.combine_with(b, sum);
-    let result_sub = a.combine_with(b, sub);
-    let result_mul = a.combine_with(b, mul);
-    let result_div = a.combine_with(b, div);
-
-    assert_eq!(result_sum, Some(15.0));
-    assert_eq!(result_sub, Some(-5.0));
-    assert_eq!(result_mul, Some(50.0));
-    assert_eq!(result_div, Some(0.5));
-    ```
-    <https://stackoverflow.com/questions/33779562/is-there-any-built-in-way-to-combine-two-options>
-    <https://docs.rs/stdext/latest/stdext/option/trait.OptionExt.html>
-    */
+    /// Combines two Options into one using a custom closure.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use claudiofsr_lib::OptionExtension;
+    ///
+    /// let a = Some(5.0);
+    /// let b = Some(10.0);
+    ///
+    /// let sum = |a, b| { a + b };
+    /// let sub = |a, b| { a - b };
+    /// let mul = |a, b| { a * b };
+    /// let div = |a, b| { a / b };
+    ///
+    /// assert_eq!(a.combine_with(b, sum), Some(15.0));
+    /// assert_eq!(a.combine_with(b, sub), Some(-5.0));
+    /// assert_eq!(a.combine_with(b, mul), Some(50.0));
+    /// assert_eq!(a.combine_with(b, div), Some(0.5));
+    /// ```
     fn combine_with<U, R, F>(self, other: Option<U>, f: F) -> Option<R>
     where
         F: Fn(T, U) -> R;
 
-    /**
-    Combine two Options with the Sum operation.
-    ```
-        use claudiofsr_lib::OptionExtension;
-
-        let a = Some(5);
-        let b = Some(10);
-
-        let result_sum = a.combine_with_sum(b);
-        assert_eq!(result_sum, Some(15));
-    ```
-    */
+    /// Combines two Options using the Addition (`+`) operator.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use claudiofsr_lib::OptionExtension;
+    ///
+    /// let a = Some(5);
+    /// let b = Some(10);
+    ///
+    /// assert_eq!(a.combine_with_sum(b), Some(15));
+    /// ```
     fn combine_with_sum<U, R>(self, other: Option<U>) -> Option<R>
     where
         T: std::ops::Add<U, Output = R>;
 
-    /**
-    Combine two Options with the Subtraction operation.
-    ```
-        use claudiofsr_lib::OptionExtension;
-
-        let a = Some(5);
-        let b = Some(10);
-
-        let result_sub = a.combine_with_sub(b);
-        assert_eq!(result_sub, Some(-5));
-    ```
-    */
+    /// Combines two Options using the Subtraction (`-`) operator.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use claudiofsr_lib::OptionExtension;
+    ///
+    /// let a = Some(5);
+    /// let b = Some(10);
+    ///
+    /// assert_eq!(a.combine_with_sub(b), Some(-5));
+    /// ```
     fn combine_with_sub<U, R>(self, other: Option<U>) -> Option<R>
     where
         T: std::ops::Sub<U, Output = R>;
 
-    /**
-    Combine two Options with the Multiplication operation.
-    ```
-        use claudiofsr_lib::OptionExtension;
-
-        let a = Some(5);
-        let b = Some(10);
-
-        let result_mul = a.combine_with_mul(b);
-        assert_eq!(result_mul, Some(50));
-    ```
-    */
+    /// Combines two Options using the Multiplication (`*`) operator.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use claudiofsr_lib::OptionExtension;
+    ///
+    /// let a = Some(5);
+    /// let b = Some(10);
+    ///
+    /// assert_eq!(a.combine_with_mul(b), Some(50));
+    /// ```
     fn combine_with_mul<U, R>(self, other: Option<U>) -> Option<R>
     where
         T: std::ops::Mul<U, Output = R>;
 
-    /**
-    Combine two Options with the Division operation.
-    ```
-        use claudiofsr_lib::OptionExtension;
-
-        let a = Some(50);
-        let b = Some(10);
-
-        let result_div = a.combine_with_div(b);
-        assert_eq!(result_div, Some(5));
-    ```
-    */
+    /// Combines two Options using the Division (`/`) operator.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use claudiofsr_lib::OptionExtension;
+    ///
+    /// let a = Some(50);
+    /// let b = Some(10);
+    ///
+    /// assert_eq!(a.combine_with_div(b), Some(5));
+    /// ```
     fn combine_with_div<U, R>(self, other: Option<U>) -> Option<R>
     where
         T: std::ops::Div<U, Output = R>;
 
-    /**
-    Converts `Option<T>` to a String.
-    ```
-        use claudiofsr_lib::OptionExtension;
-
-        let a: Option<&str> = Some("foo bar");
-        let b: Option<u16> = Some(50);
-        let c: Option<f64> = Some(10.00);
-        let d: Option<f64> = Some(10.700);
-        let e: Option<f32> = Some(0.0);
-        let f: Option<f32> = Some(0.00000);
-        let g: Option<usize> = None;
-
-        let mut strings = Vec::new();
-
-        strings.push(a.to_string());
-        strings.push(b.to_string());
-        strings.push(c.to_string());
-        strings.push(d.to_string());
-        strings.push(e.to_string());
-        strings.push(f.to_string());
-        strings.push(g.to_string());
-
-        assert_eq!(
-            strings,
-            [
-                "foo bar",
-                "50",
-                "10.0",
-                "10.7",
-                "0.0",
-                "0.0",
-                "",
-            ]
-        );
-    ```
-    */
+    /// Converts `Option<T>` to a `String`, with safe float truncation prevention.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use claudiofsr_lib::OptionExtension;
+    ///
+    /// let a: Option<&str> = Some("foo bar");
+    /// let b: Option<u16> = Some(50);
+    /// let c: Option<f64> = Some(10.00);
+    /// let d: Option<f64> = Some(10.700);
+    /// let e: Option<f32> = Some(0.0);
+    /// let f: Option<f32> = Some(0.00000);
+    /// let g: Option<usize> = None;
+    ///
+    /// assert_eq!(a.to_string(), "foo bar");
+    /// assert_eq!(b.to_string(), "50");
+    /// assert_eq!(c.to_string(), "10.0");
+    /// assert_eq!(d.to_string(), "10.7");
+    /// assert_eq!(e.to_string(), "0.0");
+    /// assert_eq!(f.to_string(), "0.0");
+    /// assert_eq!(g.to_string(), "");
+    /// ```
     fn to_string(&self) -> String;
 
-    /**
-    Parse `Option<T>` to `Option<U>`
-    with leading and trailing whitespace removed.
-    ```
-        use claudiofsr_lib::OptionExtension;
-
-        let a: Option<u64> = Some(56);
-        let b: Option<&str> = Some(" 56 ");
-        let c: Option<&str> = Some(" foo bar \n");
-        let d: Option<String> = Some("379.32000".to_string());
-        let e: Option<String> = Some("379.32   ".to_string());
-
-        let result_a: Option<u8> = a.parse();
-        let result_b: Option<u8> = b.parse();
-        let result_c: Option<String> = c.parse();
-        let result_d: Option<f32> = d.parse();
-        let result_e: Option<f64> = e.parse();
-        let result_f: Option<u64> = e.parse();
-
-        assert_eq!(result_a, Some(56));
-        assert_eq!(result_b, Some(56));
-        assert_eq!(result_c, Some("foo bar".to_string()));
-        assert_eq!(result_d, Some(379.32));
-        assert_eq!(result_e, Some(379.32));
-        assert_eq!(result_f, None);
-    ```
-    */
-    fn parse<U>(&self) -> Option<U>
+    /// Parses an `Option<T>` into an `Option<U>` after stripping whitespace.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use claudiofsr_lib::OptionExtension;
+    ///
+    /// let a: Option<u64> = Some(56);
+    /// let b: Option<&str> = Some(" 56 ");
+    /// let c: Option<&str> = Some(" foo bar \n");
+    /// let d: Option<String> = Some("379.32000".to_string());
+    /// let e: Option<String> = Some("379.32   ".to_string());
+    ///
+    /// let result_a: Option<u8> = a.parse_opt();
+    /// let result_b: Option<u8> = b.parse_opt();
+    /// let result_c: Option<String> = c.parse_opt();
+    /// let result_d: Option<f32> = d.parse_opt();
+    /// let result_e: Option<f64> = e.parse_opt();
+    /// let result_f: Option<u64> = e.parse_opt();
+    ///
+    /// assert_eq!(result_a, Some(56));
+    /// assert_eq!(result_b, Some(56));
+    /// assert_eq!(result_c, Some("foo bar".to_string()));
+    /// assert_eq!(result_d, Some(379.32));
+    /// assert_eq!(result_e, Some(379.32));
+    /// assert_eq!(result_f, None);
+    /// ```
+    fn parse_opt<U>(&self) -> Option<U>
     where
         U: std::str::FromStr;
 
-    /**
-    Retain only digits from `Option<T>`.
-
-    Get a reference to the underlying String from the Option.
-
-    If the option contains a string, transform it into an optional string containing
-    only the ASCII digit characters. Otherwise, return None.
-    ```
-        use claudiofsr_lib::OptionExtension;
-
-        // empty string
-        let opt_str: Option<&str> = None;
-        assert_eq!(opt_str.retain_only_digits(), None);
-
-        // non digit characters
-        let opt_str: Option<&str> = Some("abc123-def;!456 @");
-        assert_eq!(opt_str.retain_only_digits(), Some("123456".to_string()));
-
-        // all digits
-        let opt_str: Option<String> = Some("0123456789".to_string());
-        assert_eq!(opt_str.retain_only_digits(), Some("0123456789".to_string()));
-
-        // no digits
-        let opt_str: Option<&str> = Some("abcdefg");
-        assert_eq!(opt_str.retain_only_digits(), None);
-    ```
-    */
+    /// Retains only ASCII digit characters from an `Option<T>`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use claudiofsr_lib::OptionExtension;
+    ///
+    /// let opt_none: Option<&str> = None;
+    /// assert_eq!(opt_none.retain_only_digits(), None);
+    ///
+    /// let opt_mixed: Option<&str> = Some("abc123-def;!456 @");
+    /// assert_eq!(opt_mixed.retain_only_digits(), Some("123456".to_string()));
+    ///
+    /// let opt_digits: Option<String> = Some("0123456789".to_string());
+    /// assert_eq!(opt_digits.retain_only_digits(), Some("0123456789".to_string()));
+    ///
+    /// let opt_letters: Option<&str> = Some("abcdefg");
+    /// assert_eq!(opt_letters.retain_only_digits(), None);
+    /// ```
     fn retain_only_digits(&self) -> Option<String>
     where
         T: Deref<Target = str>;
@@ -202,6 +174,7 @@ impl<T> OptionExtension<T> for Option<T>
 where
     T: Display,
 {
+    #[inline]
     fn combine_with<U, R, F>(self, other: Option<U>, f: F) -> Option<R>
     where
         F: Fn(T, U) -> R,
@@ -212,85 +185,56 @@ where
         self.zip(other).map(|(x, y)| f(x, y))
     }
 
+    #[inline]
     fn combine_with_sum<U, R>(self, other: Option<U>) -> Option<R>
     where
         T: std::ops::Add<U, Output = R>,
     {
-        let addition = |a, b| a + b;
-        self.combine_with(other, addition)
+        self.combine_with(other, |a, b| a + b)
     }
 
+    #[inline]
     fn combine_with_sub<U, R>(self, other: Option<U>) -> Option<R>
     where
         T: std::ops::Sub<U, Output = R>,
     {
-        let subtraction = |a, b| a - b;
-        self.combine_with(other, subtraction)
+        self.combine_with(other, |a, b| a - b)
     }
 
+    #[inline]
     fn combine_with_mul<U, R>(self, other: Option<U>) -> Option<R>
     where
         T: std::ops::Mul<U, Output = R>,
     {
-        let multiplication = |a, b| a * b;
-        self.combine_with(other, multiplication)
+        self.combine_with(other, |a, b| a * b)
     }
 
+    #[inline]
     fn combine_with_div<U, R>(self, other: Option<U>) -> Option<R>
     where
         T: std::ops::Div<U, Output = R>,
     {
-        let division = |a, b| a / b;
-        self.combine_with(other, division)
+        self.combine_with(other, |a, b| a / b)
     }
 
     fn to_string(&self) -> String {
-        /*
-        self
-            .iter()
-            .map(|type_t| {
-                type_t.to_string()
-            })
-            .collect()
-        */
-
-        /*
-        match self {
-            Some(type_t) => type_t.to_string(),
-            None => "".to_string(),
-        }
-        */
-
-        // Avoid converting
-        // float "0.0"
-        // to
-        // integer "0"
-
         match self {
             Some(type_t) => {
                 let type_name = std::any::type_name::<T>();
-                //println!("type_name: {type_name}");
-                match type_name {
-                    "f32" | "f64" => {
-                        if let Ok(float_64) = type_t.to_string().parse::<f64>() {
-                            // Check if a float can be converted to integer without loss
-                            if float_64.trunc() == float_64 {
-                                format!("{float_64:.1}")
-                            } else {
-                                float_64.to_string()
-                            }
-                        } else {
-                            type_t.to_string()
-                        }
-                    }
-                    _ => type_t.to_string(),
+                if (type_name == "f32" || type_name == "f64")
+                    && let Ok(float_64) = type_t.to_string().parse::<f64>()
+                    && float_64.trunc() == float_64
+                {
+                    return format!("{float_64:.1}");
                 }
+                type_t.to_string()
             }
-            None => "".to_string(),
+            None => String::new(),
         }
     }
 
-    fn parse<U>(&self) -> Option<U>
+    #[inline]
+    fn parse_opt<U>(&self) -> Option<U>
     where
         U: std::str::FromStr,
     {
@@ -298,22 +242,14 @@ where
             .and_then(|entry| entry.to_string().trim().parse::<U>().ok())
     }
 
+    #[inline]
     fn retain_only_digits(&self) -> Option<String>
     where
         T: Deref<Target = str>,
     {
-        self.as_ref().and_then(|text| {
-            let digits: String = text
-                .chars()
-                .filter(|c| c.is_ascii_digit())
-                .collect::<String>();
-
-            if digits.is_empty() {
-                None
-            } else {
-                Some(digits)
-            }
-        })
+        let text = self.as_deref()?;
+        let digits: String = text.chars().filter(char::is_ascii_digit).collect();
+        (!digits.is_empty()).then_some(digits)
     }
 }
 
